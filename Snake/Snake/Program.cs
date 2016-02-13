eb63beb084ec9d782996b8f7b6d75c67f3516de5
bigol17 <psch_olga@mail.ru>
2016-02-13 23:41:32 +0300

    Созданы классы горизонтальных и вертикальных линий. Класс фигура. Наследование

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,73 +10,84 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            /*  Point p1 = new Point( 1, 3, '*');
-              p1.Draw();
+            /* Point p1 = new Point( 1, 3, '*');
+             p1.Draw();
 
-              Point p2 = new Point( 4, 5, '#');
-              p2.Draw();
-
-
-              List<int> numList = new List<int>();
-
-              numList.Add(0);
-              numList.Add(1);
-              numList.Add(2);
-
-              int x = numList[0];
-              int y = numList[1];
-              int z = numList[2];
-
-              foreach ( int i in numList)
-              {
-                  Console.WriteLine( i);
-              }
-
-              numList.RemoveAt(0);
-
-              List<Point> pList = new List<Point>();
-              pList.Add(p1);
-              pList.Add(p2);
-
-              Console.WriteLine();
-
-              foreach ( Point j in pList)
-              {
-
-                  Console.WriteLine(j.x + "!" + j.y + "!" + j.sym + "!!!!"); 
-              }
-
-              Console.ReadLine(); */
-
-            List<string> strList = new List<string>();
-
-            strList.Add("*");
-            strList.Add("#");
-            strList.Add("$");
-
-            foreach (string i in strList)
-            {
-                Console.WriteLine(i + "");
-            }
+             Point p2 = new Point( 4, 5, '#');
+             p2.Draw();
 
 
-            Point p1 = new Point(1, 1, '*');
-            //p1.Draw();
+             List<int> numList = new List<int>();
 
-            Point p2 = new Point(4, 1, '#');
-            //p2.Draw();
+             numList.Add(0);
+             numList.Add(1);
+             numList.Add(2);
 
-            Point p3 = new Point(10, 5, '&');
-            //p3.Draw();
+             int x = numList[0];
+             int y = numList[1];
+             int z = numList[2];
 
-            Point p4 = new Point(14, 15, '@');
-            //p4.Draw();
+             foreach ( int i in numList)
+             {
+                 Console.WriteLine( i);
+             }
 
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-            pList.Add(p3);
-            pList.Add(p4);
+             numList.RemoveAt(0);
+
+             List<Point> pList = new List<Point>();
+             pList.Add(p1);
+             pList.Add(p2);
+
+             Console.WriteLine();
+
+             foreach ( Point j in pList)
+             {
+
+                 Console.WriteLine(j.x + "!" + j.y + "!" + j.sym + "!!!!"); 
+             }
+
+             Console.ReadLine(); 
+
+           List<string> strList = new List<string>();
+
+           strList.Add("*");
+           strList.Add("#");
+           strList.Add("$");
+
+           foreach (string i in strList)
+           {
+               Console.WriteLine(i + "");
+           }
+
+
+           Point p1 = new Point(1, 1, '*');
+           //p1.Draw();
+
+           Point p2 = new Point(4, 1, '#');
+           //p2.Draw();
+
+           Point p3 = new Point(10, 5, '&');
+           //p3.Draw();
+
+           Point p4 = new Point(14, 15, '@');
+           //p4.Draw();
+
+           List<Point> pList = new List<Point>();
+           pList.Add(p1);
+           pList.Add(p2);
+           pList.Add(p3);*/
+
+            Console.SetBufferSize(80, 25);
+
+            HorizontalLine upLine = new HorizontalLine(1, 77, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(1, 77, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
 
             Console.ReadLine();
         }
